7d9d43dc4da3ee7f0961db6d12b83fb8c80a2372
= <=>
2024-04-26 21:46:35 +0600

    class two

---
+++ index.php
@@ -0,0 +1,77 @@
+<?php
+
+
+
+//$name = "Md Zahid";
+//$age = 27;
+//$gender = "Male";
+//
+//
+//$name = "Md Zahidul";
+//$name .= " Islam";
+//
+//
+////echo "Name: $name";
+//echo "Name: {$name}";
+
+//$name ="";
+//$name = "md";
+//
+//echo "$name";
+
+//const NAME = 'Md Zahidul Islam';
+//
+//
+//$callConstant = 'constant';
+////$name = constant('NAME');
+// echo $callConstant('NAME');
+//echo $name;
+
+//echo "<pre>";
+//var_dump($_SERVER);
+
+//$name = "Md Zahid" . PHP_EOL;
+//$x = 'it\'s my pen' . PHP_EOL;
+//
+//echo $x;
+//
+//echo $name;
+//echo PHP_EOL;
+//echo "\n";
+//echo "<br>";
+//print $name;
+
+
+// $name = "This is my name";
+
+/*
+    $name = "";
+    $age = 27;
+    $email = "mdzahid.pro@gmail.com";
+*/
+
+# this is a single line comment
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    $name = "Md Zahidul Islam";
+//
+//    echo "My name is " . $name;
+
+
+//$x = 10;
+//$y = 20;
+//
+//$x = 100;
+//
+//echo $x + $y;
