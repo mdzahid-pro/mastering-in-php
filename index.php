b743f6c5d4320299246d973ec5240382760a94d7
= <=>
2024-05-18 00:47:40 +0600

    class eight end

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 // drawing square shape
-$size = 5;
+$size = 0;
 
 for ($row = 0; $row < $size; $row++) {
     for ($col = 0; $col < $size; $col++) {
@@ -20,7 +22,7 @@ for ($row = 0; $row < $size; $row++) {
     echo "\n";
 }
 
-echo "\n=========================== \n\n";
+//echo "\n=========================== \n\n";
 
 // drawing an inverted right triangle
 for ($row = $size; $row > 0; $row--) {
@@ -32,7 +34,7 @@ for ($row = $size; $row > 0; $row--) {
 }
 
 
-echo "\n=========================== \n\n";
+//echo "\n=========================== \n\n";
 
 // drawing a pyramid
 for ($row = 1; $row <= $size; $row++) {
@@ -62,3 +64,102 @@ for ($row = $size -1; $row > 0; $row--) {
 
     echo "\n";
 }
+
+
+
+
+
+// function in php
+
+// User defined function
+function sum(int $a,int $b): int
+{
+    return $a + $b;
+}
+
+//echo sum(10, '20') . "\n";
+// anonymous function
+$greet = function ($name){
+    return "Hello $name";
+};
+
+//echo $greet("Md Zahid") . "\n";
+// built in function
+//echo strlen($greet("Md Zahid"));
+// arrow function
+$numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+$squared = array_map(fn($n) => $n ** 2, $numbers);
+
+//print_r($squared);
+// conditional function
+$makeGreeting = true;
+
+if($makeGreeting){
+    function greeting(string $name): string
+    {
+        return "Hello $name";
+    }
+}
+
+//echo "\n";
+//echo greeting("Md Zahid");
+
+function pyramid(int $size,bool $isRight = true): void
+{
+    if($isRight):
+        // drawing a pyramid
+        for ($row = 1; $row <= $size; $row++) {
+            // print spaces before start
+            for ($space = $size - $row; $space > 0; $space--){
+                echo "  ";
+            }
+
+            // print starts after spaces
+            for ($col = 1; $col <= (2 * $row - 1); $col++){
+                echo "* ";
+            }
+
+            echo "\n";
+        }
+    else:
+        // drawing a diamond
+        for ($row = $size -1; $row > 0; $row--) {
+            for ($space = 0; $space < $size - $row; $space++) {
+                echo "  ";
+            }
+
+            for ($col = 1; $col <= (2 * $row - 1); $col++){
+                echo "* ";
+            }
+
+            echo "\n";
+        }
+    endif;
+}
+
+
+function makeDiamond(int $size): void
+{
+    pyramid($size);
+    pyramid($size, false);
+}
+
+pyramid(10, false);
+makeDiamond(10);
+pyramid(10);
+
+
+$array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+print_r($array);
+
+$obj = (new stdClass);
+
+print_r($obj);
+
+
+
+
+
+
+
