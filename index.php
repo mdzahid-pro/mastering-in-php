4a619e4adad9390c0d77cbafbf528b38201e0d7c
Md Zahid <77210189+mdzahid-pro@users.noreply.github.com>
2024-06-29 23:15:02 +0600

    Error handeling in php

--- index.php
+++ index.php
@@ -1,25 +1,27 @@
 <?php
 
-// working with unix datetime or timestamp
+ini_set('display_errors', 1);
 
-$current_timestamp = time();
-//echo $current_timestamp . "<br>";
-//echo $current_timestamp + 86400 . "<br>"; // add one day
-//echo $current_timestamp - 86400 . "<br>"; // subtract one day
-//echo $current_timestamp + 7 * 24 * 60 * 60 . "<br>"; // add 7 days
+error_reporting(E_ALL);
 
-//echo date_default_timezone_get() ."<br>";
-date_default_timezone_set("Asia/Dhaka");
+trigger_error('This is an custom error', E_USER_ERROR);
 
-// format unix timestamp
-$formatted_date = date("Y-F-D h:i:sa", $current_timestamp) . "<br>";
 
-// add 5 days from current time by using mktime
-$timestamp = mktime(0,0,0, 6,26, 2050);
-//echo date("Y-m-d H:i:s", $timestamp) . "<br>";
-//echo "<pre>";
-//print_r(date_parse(date("Y-m-d H:i:s", $timestamp)));
+echo $e;
 
-$timestamp = strtotime("2024-12-31");
+function customErrorHandler(
+    int $error,
+    string $msg,
+    ?string $file,
+    ?int $line
+){
+    echo "Custom Error: $msg in $file on line $line";
 
-echo date("m-y-d", $timestamp) . "<br>";
+    exit;
+}
+
+error_reporting(E_ALL && ~E_USER_WARNING);
+
+set_error_handler("customErrorHandler", E_ALL);
+
+echo $e;
